d0052ba8df39d7bd7ee329c2848a68f5b65544aa
Tuan Nguyen <titan.ngvtuan@gmail.com>
2022-01-24 13:38:47 +0700

    Fix the install endpoint

--- www/lib/constants.ts
+++ www/lib/constants.ts
@@ -4,12 +4,14 @@ export const SITE_TITLE = 'The Open Source Declarative Data Generator'
 
 export const SITE_DESCRIPTION = 'Generate test data programmatically. Need fake data for application testing or anything else? Just use Synth.'
 
-export const SITE_URL = 'https://getsynth.com/'
+export const SITE_URL = 'https://sth-test.vercel.app/'
 
 export const TWITTER_HANDLE = '@getsynth'
 
-export const SITE_DOMAINS = ['www.getsynth.com', 'getsynth.com']
+export const SITE_DOMAINS = ['www.sth-test.vercel.app', 'sth-test.vercel.app']
 
-export const POSTHOG_API_KEY = 'L-AQtrFVtZGL_PjK2FbFLBR3oXNtfv8OrCD8ObyeBQo'
+// export const POSTHOG_API_KEY = 'L-AQtrFVtZGL_PjK2FbFLBR3oXNtfv8OrCD8ObyeBQo'
+// For test
+export const POSTHOG_API_KEY = 'phc_W8R9PsDtXBRnRF93uaIa18BKEbkBxURV0l5NbQSySSB'
 
 export const FATHOM_API_KEY = 'QRVYRJEG'
